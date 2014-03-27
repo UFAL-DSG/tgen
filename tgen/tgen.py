730dfaa1cc971dc0cc72a6ab284d3cb748e495f1
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2014-03-27 12:20:50 +0100

    Renaming

--- tgen/tgen.py
+++ tgen/tgen.py
@@ -4,12 +4,11 @@
 """
 Generating random t-trees.
 
-Usage: ./tgen_random train-das train-t-trees test-das output-t-trees
+Usage: ./tgen_random train_generator-das train_generator-t-trees test-das output-t-trees
 """
 
 from __future__ import unicode_literals
-from alex.components.nlg.tectotpl.core.node import T
-from alex.components.nlg.tectotpl.core.document import Document, Bundle, Zone
+from alex.components.nlg.tectotpl.core.document import Document
 from alex.components.nlg.tectotpl.block.write.yaml import YAML as YAMLWriter
 from alex.components.nlg.tectotpl.block.read.yaml import YAML as YAMLReader
 from alex.components.slu.da import DialogueAct
@@ -18,21 +17,31 @@ from flect.logf import log_info
 
 import random
 from collections import deque, defaultdict, Counter
-from getopt import getopt
 import sys
 import cPickle as pickle
 
-class RandomTTreeGenerator(object):
+
+class TTreeGenerator(object):
+    """Random t-tree generator given DAs.
+
+    Trainable from DA distributions
+    """
 
     MAX_TREE_SIZE = 50
 
-    def __init__(self, language='en', selector='', probs=None):
+    def __init__(self, language='en', selector=''):
+        """Initialize the generator (just language and selector, distributions are empty)"""
         self.language = language
         self.selector = selector
         self.form_counts = None
         self.child_cdfs = None
 
     def generate_tree(self, da, doc=None):
+        """Generate one tree given DA.
+
+        If doc is None, will create a new Treex/TectoTpl document. If doc is set, will
+        add to the end of the given document.
+        """
         # create a document
         if doc is None:
             doc = Document()
@@ -54,6 +63,7 @@ class RandomTTreeGenerator(object):
         return doc
 
     def __generate_child(self, parent, cdf):
+        """Generate one t-node, given its parent and the CDF for the possible children."""
         child = parent.create_child()
         formeme, t_lemma, right = self.sample(cdf)
         child.t_lemma = t_lemma
@@ -64,7 +74,11 @@ class RandomTTreeGenerator(object):
             child.shift_before_subtree(parent)
         return child
 
-    def train(self, da_file, t_file):
+    def train_generator(self, da_file, t_file):
+        """``Train'' the generator (collect counts of DAs and corresponding t-nodes).
+
+        Will load a Treex YAML file or a pickle (to speed up loading of YAML).
+        """
         # read training data
         log_info('Reading ' + t_file)
         if 'pickle' in t_file:
@@ -76,7 +90,7 @@ class RandomTTreeGenerator(object):
             fh = file_stream(pickle_file, mode='wb', encoding=None)
             pickle.dump(ttrees, fh, pickle.HIGHEST_PROTOCOL)
         log_info('Reading ' + da_file)
-        das = self.read_das(da_file)
+        das = read_das(da_file)
         # collect counts
         log_info('Collecting counts')
         form_counts = defaultdict(lambda: defaultdict(Counter))
@@ -93,18 +107,8 @@ class RandomTTreeGenerator(object):
         self.form_counts = form_counts
         self.child_cdfs = self.cdfs_from_counts(child_counts)
 
-    def read_das(self, da_file):
-        """Read dialogue acts from a file, one-per-line."""
-        # TODO: move outside
-        das = []
-        with file_stream(da_file) as fh:
-            for line in fh:
-                da = DialogueAct()
-                da.parse(line)
-                das.append(da)
-        return das
-
     def get_merged_cdfs(self, da):
+        """Get merged CDFs for the DAIs in the given DA."""
         merged_counts = defaultdict(Counter)
         for dai in da:
             for parent_formeme in self.form_counts[dai]:
@@ -112,6 +116,7 @@ class RandomTTreeGenerator(object):
         return self.cdfs_from_counts(merged_counts)
 
     def cdfs_from_counts(self, counts):
+        """Given a dictionary of counts, create a dictionary of corresponding CDFs."""
         cdfs = {}
         for key in counts:
             tot = 0
@@ -123,29 +128,44 @@ class RandomTTreeGenerator(object):
         return cdfs
 
     def sample(self, cdf):
-        max = cdf[-1][1]
-        rand = random.random() * max
+        """Return a sample from the distribution, given a CDF (as a list)."""
+        total = cdf[-1][1]
+        rand = random.random() * total  # get a random number in [0,total)
         for key, ubound in cdf:
             if ubound > rand:
                 return key
         raise Exception('Unable to generate from CDF!')
 
 
+def read_das(da_file):
+    """Read dialogue acts from a file, one-per-line."""
+    das = []
+    with file_stream(da_file) as fh:
+        for line in fh:
+            da = DialogueAct()
+            da.parse(line)
+            das.append(da)
+    return das
+
+
 if __name__ == '__main__':
+
+    random.seed(1206)
+
     files = sys.argv[1:]
     if len(files) != 4:
         sys.exit(__doc__)
     fname_da_train, fname_ttrees_train, fname_da_test, fname_ttrees_out = files
 
-    # initialize, train
+    # initialize, train_generator
     log_info('Initializing...')
-    tgen = RandomTTreeGenerator()
-    tgen.train(fname_da_train, fname_ttrees_train)
+    tgen = TTreeGenerator()
+    tgen.train_generator(fname_da_train, fname_ttrees_train)
 
     # generate
     log_info('Generating...')
     doc = None
-    das = tgen.read_das(fname_da_test)
+    das = read_das(fname_da_test)
     for da in das:
         doc = tgen.generate_tree(da, doc)
 
@@ -153,4 +173,3 @@ if __name__ == '__main__':
     log_info('Writing output...')
     writer = YAMLWriter(scenario=None, args={'to': fname_ttrees_out})
     writer.process_document(doc)
-
