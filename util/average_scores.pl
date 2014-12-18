f635995ebf3909570c8a3fa50528ee30d8e19c53
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2014-12-18 17:19:12 +0100

    Fix to allow averaging files with multiple values of the same type

--- util/average_scores.pl
+++ util/average_scores.pl
@@ -2,34 +2,50 @@
 
 use strict;
 use warnings;
+use autodie;
 
 my @patterns = ( 'BEST', 'NODE', 'DEP', 'NIST score ' );
 my %data;
 my %lines;
 
+die("Usage: ./$0 file1.log file2.log [...]\n") if (!@ARGV);
+
 # collect data
-while ( my $line = <> ) {
-    chomp $line;
-    foreach my $pattern (@patterns) {
-        if ( $line =~ /$pattern/ ) {
+foreach my $file (@ARGV){
 
-            if ( !$lines{$pattern} ) {
-                $lines{$pattern} = $line;
-            }
+    open( my $fh, '<:utf8', $file );
+    my %cur_data = ();
 
-            # extract all numbers on the line
-            my @nums = $line =~ m/[0-9]+\.[0-9]+/g;
-            if ( !$data{$pattern} ) {
-                $data{$pattern} = [];
+    while ( my $line = <$fh> ) {
+        chomp $line;
+        foreach my $pattern (@patterns) {
+            if ( $line =~ /$pattern/ ) {
+
+                if ( !$lines{$pattern} ) {
+                    $lines{$pattern} = $line;
+                }
+                # extract all numbers on the line
+                my @nums = $line =~ m/[0-9]+\.[0-9]+/g;
+                # store them, keep only the last ones for this file
+                $cur_data{$pattern} = \@nums;
             }
-            push @{ $data{$pattern} }, \@nums;
         }
     }
+
+    # add to global list
+    while (my ($p, $n) = each %cur_data){
+        if ( !$data{$p} ) {
+            $data{$p} = [];
+        }
+        push $data{$p}, $n;
+    }
+    close($fh);
 }
 
 foreach my $pattern (@patterns) {
     
     my $values = $data{$pattern};
+    next if (!$values);
     
     # average data
     my $cumul = shift @$values;
