99b0e60a80ae0fa6b9024e1ebe04ad6a0a3ebf73
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2015-02-09 13:17:07 +0100

    Adding information on DAIs->nodes, DAIs->lemmas influence
    For DAI compatibility examination

--- util/inspect_data.py
+++ util/inspect_data.py
@@ -2,15 +2,15 @@
 #
 # Training data inspection. Currently prints only tree sizes
 
-from tgen.futil import read_ttrees, trees_from_doc
+from tgen.futil import read_ttrees, trees_from_doc, read_das
 import sys
 from getopt import getopt
 from collections import defaultdict
 
 opts, files = getopt(sys.argv[1:], 'l:s:')
 
-if len(files) != 1:
-    sys.exit('Usage: python inspect_data.py [-l lang] [-s selector] <file.yaml>')
+if len(files) != 2:
+    sys.exit('Usage: python inspect_data.py [-l lang] [-s selector] <trees.yaml> <das.txt>')
 
 language = 'en'
 selector = ''
@@ -22,6 +22,10 @@ for opt, arg in opts:
         selector = arg
 
 trees = trees_from_doc(read_ttrees(files[0]), language, selector)
+das = read_das(files[1])
+
+
+# TREE SIZES
 
 tree_sizes = defaultdict(int)
 for tree in trees:
@@ -29,4 +33,37 @@ for tree in trees:
 
 print "Tree sizes:\n==========="
 for k, v in sorted(tree_sizes.items()):
-    print k, "\t:", v
+    print k, "\t", v
+
+# DAS -> NODES
+
+das_for_nodes = {}
+num_occ_nodes = defaultdict(int)
+for tree, da in zip(trees, das):
+    for node in tree.nodes:
+        num_occ_nodes[node] += 1
+        if node not in das_for_nodes:
+            das_for_nodes[node] = set(da.dais)
+        else:
+            das_for_nodes[node] &= set(da.dais)
+
+print "\n\nDAs for nodes:\n=========="
+for node, dais in sorted(das_for_nodes.items()):
+    print node.t_lemma, " ", node.formeme, "\t", num_occ_nodes[node], "\t", '&'.join([unicode(dai) for dai in dais])
+
+# DAS -> LEMMAS
+
+das_for_lemmas = {}
+num_occ_lemmas = defaultdict(int)
+for tree, da in zip(trees, das):
+    for node in tree.nodes:
+        lemma = unicode(node.t_lemma).lower()
+        num_occ_lemmas[lemma] += 1
+        if lemma not in das_for_lemmas:
+            das_for_lemmas[lemma] = set(da.dais)
+        else:
+            das_for_lemmas[lemma] &= set(da.dais)
+
+print "\n\nDAs for lemmas:\n=========="
+for lemma, dais in sorted(das_for_lemmas.items()):
+    print lemma, "\t", num_occ_lemmas[lemma], "\t", '&'.join([unicode(dai) for dai in dais])
