a13e57e235ed2775503111d96e1a5c01fe0b77d9
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2015-04-09 15:25:18 +0200

    Removing debug prints

--- tgen/rnd.py
+++ tgen/rnd.py
@@ -1,29 +1,13 @@
-# ##
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# import functools
-# import inspect
-# import traceback
-# import sys
-#
-# def monitor(f):
-#     @functools.wraps(f)
-#     def decorated(*args, **kwargs):
-#         print >> sys.stderr, "*** RND {}: {}, {}".format(f.__name__, args, kwargs)
-#         # traceback.print_stack()
-#         # Do whatever you want: increment some counter, etc.
-#         return f(*args, **kwargs)
-#     return decorated
+"""
+My own random number generator instance which is used instead of the system one to prevent
+Theano from interfering with it.
+
+"""
 
 from random import Random
-
-# for name in dir(random):
-#     f = getattr(random, name)
-#     if inspect.isfunction(f) or inspect.ismethod(f) and not name.startswith('_'):
-#         log_info('Wrapping ' + name)
-#         setattr(random, name, monitor(f))
-
-# ##
-
 
 rnd = Random()
 rnd.seed(1206)
