6a46ef4d873c49dfcdf8becd5def0151e1334f56
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2015-05-12 15:41:45 +0200

    Various debugging functions

---
+++ tgen/debug.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Various debugging utilities.
+
+Sources:
+
+Starting IPdb automatically on error:
+- http://stackoverflow.com/questions/242485/starting-python-debugger-automatically-on-error
+- https://github.com/ticcky/code101/blob/master/pdb_on_error.py
+
+Inspecting Theano functions:
+- http://www.deeplearning.net/software/theano/tutorial/debug_faq.html
+"""
+
+import sys
+
+
+def exc_info_hook(exc_type, value, tb):
+    """An exception hook that starts IPdb automatically on error if in interactive mode."""
+
+    if hasattr(sys, 'ps1') or not sys.stderr.isatty():
+        # we are in interactive mode or we don't have a tty-like
+        # device, so we call the default hook
+        sys.__excepthook__(exc_type, value, tb)
+    else:
+        import traceback
+        import ipdb
+        # we are NOT in interactive mode, print the exception
+        traceback.print_exception(exc_type, value, tb)
+        print
+        # then start the debugger in post-mortem mode.
+        # pdb.pm() # deprecated
+        ipdb.post_mortem(tb)  # more modern
+
+
+def inspect_inputs(i, node, fn):
+    """Inspecting Theano function graph in MonitorMode: print inputs."""
+    print i, node, "IN:", [inp[0] for inp in fn.inputs],
+
+
+def inspect_outputs(i, node, fn):
+    """Inspecting Theano function graph in MonitorMode: print outputs."""
+    print "OUT:", [output[0] for output in fn.outputs]
+
+
+def inspect_input_dims(i, node, fn):
+    """Inspecting Theano function graph in MonitorMode: print inputs' dimensions."""
+    print i, node, "IN:", [inp[0].shape for inp in fn.inputs],
+
+
+def inspect_output_dims(i, node, fn):
+    """Inspecting Theano function graph in MonitorMode: print outputs' dimensions."""
+    print "OUT:", [output[0].shape for output in fn.outputs]
