bc0a634be4d0f9d8612540cd859ca263883636f9
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2015-11-13 17:20:37 +0100

    Config file for the new NN classifier

---
+++ bagel-data/config/classif.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+
+config = {'language': 'en',
+          'selector': '',
+          'nn_shape': 'ff1',
+          'num_hidden_units': 128,
+          'initialization': 'norm_sqrt',
+          'passes': 200,
+          'randomize': True,
+          }
