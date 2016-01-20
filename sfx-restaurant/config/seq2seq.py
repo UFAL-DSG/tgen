05e6955fc294f84331c851c8d1d59ddaaedfdd4b
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2016-01-20 17:29:39 +0100

    Copying seq2seq config for SFX data

---
+++ sfx-restaurant/config/seq2seq.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+config = {
+          'alpha': 1e-3,
+          'randomize': True,
+          'passes': 200,
+          'num_hidden_units': 128,
+          'emb_size': 50,
+          'batch_size': 20,
+          'validation_freq': 10,
+          'validation_size': 10,
+          'max_cores': 4,
+          'use_tokens': False,
+          'nn_type': 'emb_seq2seq',
+          }
