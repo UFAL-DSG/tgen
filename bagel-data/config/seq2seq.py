74788ea286231971c25948f3aa38411182c8db7f
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2016-02-17 15:43:33 +0100

    Config file (forgotten commit)

---
+++ bagel-data/config/seq2seq.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+config = {
+          'alpha': 1e-3,
+          'randomize': True,
+          'num_hidden_units': 128,
+          'emb_size': 50,
+          'batch_size': 30,
+          'max_cores': 4,
+          'use_tokens': False,
+          'nn_type': 'emb_attention_seq2seq',
+          'sort_da_emb': True,
+          'cell_type': 'lstm',
+          'dropout_keep_prob': 1,
+
+          'validation_size': 10,
+          'validation_freq': 1,
+          'passes': 1000,
+          'min_passes': 100,
+          'improve_interval': 100,
+          'top_k': 10,
+          'bleu_validation_weight': 0,
+          }
