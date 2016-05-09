3b5b50b5d4692a24fbfc0dc2097c6a1f0ffec785
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2016-05-09 15:22:26 +0200

    This actually seems to work well

--- sfx-restaurant/config/seq2seq.py
+++ sfx-restaurant/config/seq2seq.py
@@ -4,7 +4,7 @@
 from __future__ import unicode_literals
 
 config = {
-          'alpha': 1e-3,
+          'alpha': 5e-4,
           'randomize': True,
           'num_hidden_units': 128,
           'emb_size': 50,
@@ -27,27 +27,27 @@ config = {
           'improve_interval': 50,
           'top_k': 10,
           'bleu_validation_weight': 1,
-          'beam_size': 1, # 20,
+          'beam_size': 20,
           'sample_top_k': 1,
-          'length_normalized_decoding': False,
+          'length_norm_weight': 0,
           'alpha_decay': 0, # 0.03
 
-          #'classif_filter': {
-              #'language': 'en',
-              #'selector': '',
-              ##'nn': '1-hot',
-              #'nn': 'emb',
-              ##'nn_shape': 'ff1',
-              #'nn_shape': 'rnn',
-              #'num_hidden_units': 128,
-              ##'passes': 200,
-              #'passes': 100,
-              #'min_passes': 20,
-              #'randomize': True,
-              #'batch_size': 20,
-              #'alpha': 1e-3,
-              #'emb_size': 50,
-              #'max_tree_len': 50,
-              #'validation_freq': 1,
-              #}
+          'classif_filter': {
+              'language': 'en',
+              'selector': '',
+              #'nn': '1-hot',
+              'nn': 'emb',
+              #'nn_shape': 'ff1',
+              'nn_shape': 'rnn',
+              'num_hidden_units': 128,
+              #'passes': 200,
+              'passes': 100,
+              'min_passes': 20,
+              'randomize': True,
+              'batch_size': 20,
+              'alpha': 1e-3,
+              'emb_size': 50,
+              'max_tree_len': 50,
+              'validation_freq': 1,
+              }
           }
