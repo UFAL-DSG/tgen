3681cd9249b296d4329289a6b97455dfb719f57a
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2016-09-28 22:08:04 +0200

    Config & postprocessing for cs-restaurant

---
+++ cs-restaurant/postprocess/postprocess.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Postprocessing for Alex Context NLG Dataset
+"""
+
+from __future__ import unicode_literals
+import pickle
+import sys
+from argparse import ArgumentParser
+import subprocess
+
+from tgen.futil import file_stream
+from tgen.debug import exc_info_hook
+sys.excepthook = exc_info_hook
+
+
+def process_file(args):
+
+    # find out generator mode
+    mode = 'trees'
+    if args.model:
+        with file_stream(args.model, 'rb', encoding=None) as fh:
+            data = pickle.load(fh)
+            if 'mode' in data['cfg']:
+                mode = data['cfg']['mode']
+            if 'use_tokens' in data['cfg']:
+                mode = 'tokens' if data['cfg']['use_tokens'] else 'trees'
+
+    # compose scenario
+
+    scen = ['T2A::CS::VocalizePrepos',  # this surface stuff is needed for both tokens and tagged lemmas
+            'T2A::CS::CapitalizeSentStart',
+            'A2W::ConcatenateTokens',
+            'A2W::CS::DetokenizeUsingRules',
+            'A2W::CS::RemoveRepeatedTokens', ]
+    if mode == 'tokens':
+        scen = ['T2A::CopyTtree',
+                'Util::Eval anode="$.set_form($.lemma);"', ] + scen
+    elif mode == 'tagged_lemmas':
+        scen = ['T2A::CopyTtree',
+                'Util::Eval atree="@as=$.get_descendants({ordered=>1}); ' +
+                'while (($l, $t) = splice @as, 0, 2){ $l->set_tag($t.lemma); $t->remove(); }"',
+                'T2A::CS::GenerateWordforms', ] + scen
+    else:
+        scen = ['T2T::AssignDefaultGrammatemes grammateme_file="%s" da_file="%s"' % (args.grammatemes, args.input_das),
+                'Scen::Synthesis::CS', ]
+
+    scen = ['Read::YAML from="%s"' % args.input_file] + scen
+    scen += ['Write::Treex',
+             'Util::Eval document="$.set_path(\"\"); $.set_file_stem(\"test\");"',
+             'Write::SgmMTEval to="%s" set_id=CsRest sys_id=TGEN add_header=tstset' % args.output_file]
+
+    subprocess.call(('treex -Lcs -S%s ' % args.selector) + " ".join(scen), shell=True)
+
+
+if __name__ == '__main__':
+    ap = ArgumentParser()
+    ap.add_argument('-m', '--model', type=str, help='Seq2seq model pickle file (to get mode)')
+    ap.add_argument('-i', '--input-das', type=str, help='Input DA file')
+    ap.add_argument('-g', '--grammatemes', type=str, help='Default grammateme file')
+    ap.add_argument('-s', '--selector', type=str, help='Target selector')
+    ap.add_argument('input_file', type=str, help='Input YAML file')
+    ap.add_argument('output_file', type=str, help='Output SGM file')
+    args = ap.parse_args()
+
+    process_file(args)
