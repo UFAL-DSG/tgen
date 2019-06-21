f79fd406949fb092433231f5d7f26e973704da12
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2019-06-22 00:21:42 +0200

    Allowing YAML config files, code cleanup

--- tgen/config.py
+++ tgen/config.py
@@ -2,19 +2,17 @@
 # -*- coding: utf-8 -*-
 
 """
-Load Python code as configuration files. A copy form Alex (http://github.com/UFAL-DSG/alex)
+Load Python code as configuration files. A copy from Alex (http://github.com/UFAL-DSG/alex)
 """
 
-from builtins import str
 from builtins import object
-import collections
-import copy
 from importlib import import_module
 import os
 import os.path
-import re
 import sys
 import tempfile
+import codecs
+import yaml
 
 config = None
 
@@ -125,110 +123,17 @@ class Config(object):
         return True
 
     def load(self, file_name):
-        # pylint: disable-msg=E0602
+        if file_name.endswith('.yaml'):
+            with codecs.open(file_name, 'r', encoding='UTF-8') as fh:
+                self.config = yaml.load(fh)
+        else:
+            # pylint: disable-msg=E0602
+            global config
+            # config = None
+            self.config = config = load_as_module(file_name, force=True).config
+            # execfile(file_name, globals())
+            # assert config is not None
+            # self.config = config
 
-        global config
-        # config = None
-        self.config = config = load_as_module(file_name, force=True).config
-        # execfile(file_name, globals())
-        # assert config is not None
-        # self.config = config
-
-        cfg_abs_dirname = os.path.dirname(os.path.abspath(file_name))
-        self.config_replace('{cfg_abs_path}', cfg_abs_dirname)
-
-        self.load_includes()
-
-    def load_includes(self):
-        if not self.contains("General", "include"):
-            return
-        for include in self["General"]["include"]:
-            self.merge(include)
-
-    def merge(self, other):
-        """Merges self's config with other's config and saves it as a new
-        self's config.
-
-        Keyword arguments:
-            - other: a Config object whose configuration dictionary to merge
-                     into self's one
-        """
-        # pylint: disable-msg=E0602
-        if type(other) is str:
-            other = Config(other)
-        self.update(other.config)
-
-    def update(self, new_config, config_dict=None):
-        """Updates the nested configuration dictionary by another, potentially
-        also nested dictionary.
-
-        Keyword arguments:
-            - new_config: the new dictionary to update with
-            - config_dict: the config dictionary to be updated
-
-        """
-        if config_dict is None:
-            config_dict = self.config
-        for key, val in new_config.items():
-            if isinstance(val, collections.Mapping):
-                subdict = self.update(val, config_dict.get(key, {}))
-                config_dict[key] = subdict
-            else:
-                config_dict[key] = new_config[key]
-        return config_dict
-
-    def config_replace(self, p, s, d=None):
-        """\
-        Replace a pattern p with string s in the whole config
-        (recursively) or in a part of the config given in d.
-
-        """
-        if d is None:
-            d = self.config
-        for k, v in d.items():
-            if isinstance(v, collections.Mapping):
-                self.config_replace(p, s, v)
-            elif isinstance(v, str):
-                d[k] = d[k].replace(p, s)
-        return
-
-    def unfold_lists(self, pattern, unfold_id_key=None, part=[]):
-        """\
-        Unfold lists under keys matching the given pattern
-        into several config objects, each containing one item.
-        If pattern is None, all lists are expanded.
-
-        Stores a string representation of the individual unfolded values
-        under the unfold_id_key if this parameter is set.
-
-        Only expands a part of the whole config hash (given by list of
-        keys forming a path to this part) if the path parameter is set.
-        """
-        # find the part of the config we're dealing with
-        dict_to_unfold = self.config
-        for key in part:
-            dict_to_unfold = dict_to_unfold[key]
-        # go through it and search for lists to unfold
-        for k, v in dict_to_unfold.items():
-            # unfold lists
-            if type(v) is list and (pattern is None or re.search(pattern, k)):
-                unfolded = []
-                for item in v:
-                    # create a copy and replace with the unfolded item
-                    ci = Config(config=copy.deepcopy(self.config))
-                    target_dict = ci
-                    for key in part:
-                        target_dict = target_dict[key]
-                    target_dict[k] = item
-                    # store the value of the unfolded items under the given key
-                    if unfold_id_key is not None:
-                        str_rep = str(item)
-                        ci[unfold_id_key] = ci[unfold_id_key] + '_' + str_rep \
-                                            if unfold_id_key in ci else str_rep
-                    # unfold other variables
-                    unfolded.extend(ci.unfold_lists(pattern, unfold_id_key))
-                return unfolded
-            # recurse deeper into dictionaries
-            elif type(v) is dict:
-                return self.unfold_lists(pattern, unfold_id_key, part + [k])
-        return [self]
+            cfg_abs_dirname = os.path.dirname(os.path.abspath(file_name))
+            self.config_replace('{cfg_abs_path}', cfg_abs_dirname)
